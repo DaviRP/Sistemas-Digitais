ceb7d42b00108c6a32f7c17100bee5351451790a
DaviRP <davi.pegoraro@hotmail.com>
2016-03-16 20:39:56 -0300

    final v2

--- PiscarVi/Top.v
+++ PiscarVi/Top.v
@@ -1,11 +1,18 @@
 module Top(
 	input clock_50,
-	output [0:0]LEDG
+	output [0:7]LEDG
 );
 	reg[31:0] cont = 0;
 	reg light_on = 1;
 
 	assign  = LEDG[0]= ~ light_on;
+	assign  = LEDG[1]= ~ light_on;
+	assign  = LEDG[2]= ~ light_on;
+	assign  = LEDG[3]= ~ light_on;
+	assign  = LEDG[4]= ~ light_on;
+	assign  = LEDG[5]= ~ light_on;
+	assign  = LEDG[6]= ~ light_on;
+	assign  = LEDG[7]= ~ light_on;
 
 	always @(posedge clk) begin
 			cont <=cont+1;
