9906d763e59d0b6e8e9dbae5de1d181d2ace8325
DaviRP <davi.pegoraro@hotmail.com>
2016-05-17 08:48:32 -0300

    preparando para testes

--- PontoEmV/Top.v
+++ PontoEmV/Top.v
@@ -18,6 +18,19 @@ module math
 endmodule
 
 
-	
+module cadTri 
+
+	(input [10:0] ponto_x,input [10:0] ponto_y,input [10:0] pt1_x, input [10:0] pt1_y, input [10:0] pt2_x,input [10:0] pt2_y, input [10:0] pt3_x,input [10:0] pt3_y, output Out_F)
 
 
+	wire x,y,z;
+
+	math i (ponto_x,ponto_y,pt1_x,pt1_y,pt2_x,pt2_y,x);
+	math j (ponto_x,ponto_y, pt2_x,pt2_y,pt3_x,pt3_y,y);
+	math k (ponto_x,ponto_y,pt3_x,pt3_y,pt1_x,pt1_y, z);
+
+
+	assign  Out_F = x= y & x=z ;
+
+
+endmodule
