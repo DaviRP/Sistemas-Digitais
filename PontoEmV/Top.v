da8b273a8cdc4f5aecb64660205cc2f7a74388d1
DaviRP <davi.pegoraro@hotmail.com>
2016-06-15 20:50:18 -0300

    FF(finalmente finalizando )

--- PontoEmV/Top.v
+++ PontoEmV/Top.v
@@ -34,3 +34,52 @@ module cadTri
 
 
 endmodule
+
+
+module   
+
+
+module teste;
+	integer Write;
+	reg [10:0]x1;
+	reg [10:0]x2;
+	reg [10:0]x3;
+	reg [10:0]y1;
+	reg [10:0]y2;
+	reg [10:0]y3;
+	reg [10:0]PointX;
+	reg [10:0]PointY;
+	reg CLK;
+	reg rst;
+	
+	Triangle A(PointX,PointY,X_1,Y_1,X_2,Y_2,X_3,Y_3,Exit);
+	
+	initial begin
+		Write = $fopen("result.txt", "w");
+		$dumpvars(0, A);
+			#0;
+				X_1 <=0;
+				Y_1 <=0;
+				X_2 <=10;
+				Y_2 <=0;
+				X_3 <=0;
+				Y_3 <=10;
+				PointX <=3;
+				PointY <=3;
+			#5;
+				$fdisplay(Write, "%d", Exit);
+			#6;
+				X_1 <=15;
+				Y_1 <=15;
+				X_2 <=30;
+				Y_2 <=0;
+				X_3 <=15;
+				Y_3 <=0;
+				PointX <=3;
+				PointY <=3;
+			#9;
+				$fdisplay(Write, "%d", Exit);
+			#10;
+		$finish;
+	end
+endmodule
